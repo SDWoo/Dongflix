89cdbdd7180bf9ec864ed69187cbbc69388440ef
sdwoo <damjang67@naver.com>
2022-12-10 03:36:39 +0900

    chore: webpack, eslint, vue, pinia, postcss 환경설정 완료

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      '~': `${__dirname}/src`,
+    },
+  },
+  entry: './src/main.js',
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: `
+                  @use "sass:color";
+                  @use "sass:list";
+                  @use "sass:map";
+                  @use "sass:math";
+                  @use "sass:meta";
+                  @use "sass:selector";
+                  @use "sass:string";
+                  @import "~/scss/_variables";
+                `,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif|webp)/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'static' }],
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
